1cf39f15867671d49a529b37a86c1c2d4ab227e6
neenu shaji <neenushaji1995@gmail.com>
2019-02-11 12:22:14 -0500

    Initial Commit

---
+++ PayrollManagement/Index.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PayrollManagement
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected override void OnPreInit(EventArgs e)
+        {
+            //Checks which user is entering the system and chooses the master pages for them
+            int postion = Model.MiscClass.position;
+            if (postion == 3)
+            {
+                MasterPageFile = "~/MasterPageAdmin.Master";
+            }
+            else if (postion == 2)
+            {
+                MasterPageFile = "~/MasterPage.Master";
+            }
+            else
+            {
+                MasterPageFile = "~/MasterPageEmp.Master";
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
