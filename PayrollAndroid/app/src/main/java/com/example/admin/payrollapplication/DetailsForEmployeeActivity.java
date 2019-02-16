25cfb10d5c033e338b9a5ef91b0084ab1fa8bab6
Aman Desai <aman.desai0406@gmail.com>
2019-02-16 18:14:24 -0500

    Added 3 new activities
    Added Activities of the pages that will display details for employee and manager

---
+++ PayrollAndroid/app/src/main/java/com/example/admin/payrollapplication/DetailsForEmployeeActivity.java
@@ -0,0 +1,13 @@
+package com.example.admin.payrollapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailsForEmployeeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee_details);
+    }
+}
