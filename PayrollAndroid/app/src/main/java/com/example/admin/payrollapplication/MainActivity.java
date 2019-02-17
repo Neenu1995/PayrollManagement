0411f8c1e12f2dbebb5ffd73000a93173c66515a
rekhapunia <44006779+rekhapunia@users.noreply.github.com>
2019-02-17 17:58:04 -0500

    Added Notification

--- PayrollAndroid/app/src/main/java/com/example/admin/payrollapplication/MainActivity.java
+++ PayrollAndroid/app/src/main/java/com/example/admin/payrollapplication/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.admin.payrollapplication;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -43,6 +45,12 @@ public class MainActivity extends AppCompatActivity {
                 addPerson();
             }
         });
+        Button startBtn = (Button) findViewById(R.id.sendEmail);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                sendEmail();
+            }
+        });
     }
 
 
@@ -62,6 +70,26 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+    protected void sendEmail() {
+        Log.i("Send email", "");
+        String[] TO = {""};
+        String[] CC = {""};
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.setType("text/plain");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+        emailIntent.putExtra(Intent.EXTRA_CC, CC);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Your subject");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "Email message goes here");
+
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+            finish();
+            Log.i("Finished sending email...", "");
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(MainActivity.this, "There is no email client installed.", Toast.LENGTH_SHORT).show();
+        }
 
 
-}
+}}
