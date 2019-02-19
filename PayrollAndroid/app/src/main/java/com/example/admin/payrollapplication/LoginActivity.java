24c06e780c85eebdab6139c9c03496ac55f53f99
FuadMannan <fuad.mannan@gmail.com>
2019-02-19 17:27:55 -0500

    Implemented Firebase Email Authentication Login

--- PayrollAndroid/app/src/main/java/com/example/admin/payrollapplication/LoginActivity.java
+++ PayrollAndroid/app/src/main/java/com/example/admin/payrollapplication/LoginActivity.java
@@ -1,13 +1,19 @@
 package com.example.admin.payrollapplication;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -19,6 +25,7 @@ public class LoginActivity extends AppCompatActivity {
     EditText passwordTextV;
     Button submitBtn;
     DatabaseReference myRef;
+    FirebaseAuth myAuth;
 
     private static final String TAG = "LoginActivity";
 
@@ -31,22 +38,43 @@ public class LoginActivity extends AppCompatActivity {
         nameTextV = findViewById(R.id.nameText);
         passwordTextV = findViewById(R.id.passwordText);
         submitBtn = findViewById(R.id.submitButton);
+        myAuth = FirebaseAuth.getInstance();
 
 
 
         submitBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View arg0) {
              //checkCredentials();
+                signIn();
 
-
-                Intent myIntent = new Intent(LoginActivity.this,
-                        MainActivity.class);
-                startActivity(myIntent);
+//                Intent myIntent = new Intent(LoginActivity.this,
+//                        MainActivity.class);
+//                startActivity(myIntent);
 
             }
         });
 
 
+    }
+
+    private void signIn(){
+        String name = nameTextV.getText().toString().trim();
+        String password = passwordTextV.getText().toString().trim();
+
+        myAuth.signInWithEmailAndPassword(name,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(LoginActivity.this,"Sign In Successfull",Toast.LENGTH_LONG).show();
+                    Intent myIntent = new Intent(LoginActivity.this,
+                            MainActivity.class);
+                    startActivity(myIntent);
+                } else {
+                    Toast.makeText(LoginActivity.this,"Sign In Failed",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
     private void checkCredentials() {
