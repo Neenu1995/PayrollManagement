cb8d6cce4052800f672fe2ac79d04f1dc3c186dc
Aman Desai <aman.desai0406@gmail.com>
2019-04-18 12:59:21 -0400

    Database connection in Calculate Pay Activity

--- PayrollAndroid/app/src/main/java/com/example/admin/payrollapplication/CalculatePayActivity.java
+++ PayrollAndroid/app/src/main/java/com/example/admin/payrollapplication/CalculatePayActivity.java
@@ -1,14 +1,23 @@
 package com.example.admin.payrollapplication;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class CalculatePayActivity extends AppCompatActivity {
+    DatabaseReference reference;
     Button btnPay;
     TextView txtviewPay;
     TextView txtviewDed;
@@ -18,6 +27,7 @@ public class CalculatePayActivity extends AppCompatActivity {
     EditText eOvtHrs;
     EditText eRegHrs;
     EditText eGrossPay;
+    EditText empId;
     double incomeTax;
     double txtgrossPay;
     double regHrs;
@@ -30,12 +40,15 @@ public class CalculatePayActivity extends AppCompatActivity {
     double totalHrs;
     double grossPay;
     double totalDeduction;
+    String employeeId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculate_pay);
+        reference = FirebaseDatabase.getInstance().getReference("employee");
 
+        empId = (EditText)findViewById(R.id.txtEmpId);
         eGrossPay = (EditText)findViewById(R.id.txtGrossPay);
         eRegHrs = (EditText)findViewById(R.id.txtRegHrs);
         eOvtHrs = (EditText)findViewById(R.id.txtOvtHrs);
@@ -47,11 +60,10 @@ public class CalculatePayActivity extends AppCompatActivity {
 
         btnPay = (Button) findViewById(R.id.btnCalculatePay);
 
-
-
         btnPay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                employeeId = empId.getText().toString();
                 txtgrossPay = Double.valueOf(eGrossPay.getText().toString());
                 regHrs = Double.valueOf(eRegHrs.getText().toString());
                 ovtHrs = Double.valueOf(eOvtHrs.getText().toString());
@@ -65,45 +77,64 @@ public class CalculatePayActivity extends AppCompatActivity {
                 calculatePay();
 
                 txtviewPay.setText(String.valueOf(netPay));
+
+                reference.orderByChild("employeeID").equalTo(employeeId).addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for (DataSnapshot ds : dataSnapshot.getChildren()){
+                            Employee temp = ds.getValue(Employee.class);
+
+                            if(temp.getEmployeeID().equals(employeeId)){
+                                ds.child("hoursWorked").getRef().setValue(totalHrs);
+                                ds.child("pay").child("end").getRef().setValue(netPay);
+                                reference.removeEventListener(this);
+                                Intent intent = new Intent(CalculatePayActivity.this,MainActivity.class);
+                                startActivity(intent);
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        Toast.makeText(CalculatePayActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
-
-
     }
 
     private double calculatePay(){
         if(grossPay > 0 && grossPay <= 47630){
             incomeTax = 0.15;
             overallDed = totalDeduction + (grossPay * incomeTax);
-            txtviewDed.setText(String.valueOf(overallDed));
+            txtviewDed.setText(String.valueOf(grossPay * incomeTax));
             netPay = grossPay - overallDed;
-
         }
         else if (grossPay > 47630 && grossPay <= 95259){
             incomeTax = 0.205;
             overallDed = totalDeduction + (grossPay * incomeTax);
-            txtviewDed.setText(String.valueOf(overallDed));
+            txtviewDed.setText(String.valueOf(grossPay * incomeTax));
             netPay = grossPay - overallDed;
             return netPay;
         }
         else if (grossPay > 95259 && grossPay <= 147667){
             incomeTax = 0.26;
             overallDed = totalDeduction + (grossPay * incomeTax);
-            txtviewDed.setText(String.valueOf(overallDed));
+            txtviewDed.setText(String.valueOf(grossPay * incomeTax));
             netPay = grossPay - overallDed;
             txtviewPay.setText(String.valueOf(netPay));
         }
         else if (grossPay > 147667 && grossPay <= 210371){
             incomeTax = 0.29;
             overallDed = totalDeduction + (grossPay * incomeTax);
-            txtviewDed.setText(String.valueOf(overallDed));
+            txtviewDed.setText(String.valueOf(grossPay * incomeTax));
             netPay = grossPay - overallDed;
             return netPay;
         }
         else if (grossPay > 210371){
             incomeTax = 0.33;
             overallDed = totalDeduction + (grossPay * incomeTax);
-            txtviewDed.setText(String.valueOf(overallDed));
+            txtviewDed.setText(String.valueOf(grossPay * incomeTax));
             netPay = grossPay - overallDed;
             return netPay;
         }
